3b625fa45236a733c00f7cc293ae65c2166a8684
cyberpower678 <cybernet678@yahoo.com>
2016-08-31 15:12:39 -0400

    Fix a bug that breaks the test cases.
    The sanitizer function improperly encoded the URL.

--- src/CheckIfDead.php
+++ src/CheckIfDead.php
@@ -383,6 +383,12 @@ class CheckIfDead {
 		if ( isset( $parts['fragment'] ) ) {
 			$url .= "#".urlencode( urldecode( $parts['fragment'] ) );
 		}
+		//Make sure path, query, and fragment are properly encoded, and not overencoded.
+		//This avoids possible 400 Bad Response errors.
+		$url .= "/";
+		if( isset( $parts['path'] ) && strlen( $parts['path'] ) > 1 ) $url .= implode( '/', array_map( "urlencode", explode( '/', substr( urldecode( $parts['path'] ), 1 ) ) ) );
+		if( isset( $parts['query'] ) ) $url .= "?".urlencode( urldecode( $parts['query'] ) );
+		if( isset( $parts['fragment'] ) ) $url .= "#".urlencode( urldecode( $parts['fragment'] ) );
 		return $url;
 	}
 
