381f4bfdfc8d4217516f19fce71594579530111f
cyberpower678 <cybernet678@yahoo.com>
2016-09-01 12:25:49 -0400

    Fix some coding and add tests
    Fixed some coding style and added some tests.

--- tests/checkIfDeadTest.php
+++ tests/checkIfDeadTest.php
@@ -1,119 +1,105 @@
 <?php
+
 require_once __DIR__ . '/../vendor/autoload.php';
 use Wikimedia\DeadlinkChecker\CheckIfDead;
 
 class CheckIfDeadTest extends PHPUnit_Framework_TestCase {
 
 	/**
-	 * Test a single dead link via HTTP
+	 * Test live links
 	 */
-	public function testDeadlinkTrueHTTP() {
+	public function testIsLinkDeadFalse() {
 		$obj = new CheckIfDead();
-		$url = 'http://worldchiropracticalliance.org/resources/greens/green4.htm';
-		$result = $obj->isLinkDead( $url );
-		$this->assertTrue( $result );
+		// @codingStandardsIgnoreStart Line exceeds 100 characters
+		$this->assertFalse( $obj->isLinkDead( 'https://en.wikipedia.org' ) );
+		$this->assertFalse( $obj->isLinkDead( '//en.wikipedia.org/wiki/Main_Page' ) );
+		$this->assertFalse( $obj->isLinkDead( 'https://en.wikipedia.org/w/index.php?title=Republic_of_India' ) );
+		$this->assertFalse( $obj->isLinkDead( 'ftp://ftp.rsa.com/pub/pkcs/ascii/layman.asc' ) );
+		$this->assertFalse( $obj->isLinkDead( 'http://www.discogs.com/Various-Kad-Jeknu-Dragačevske-Trube-2/release/1173051' ) );
+		$this->assertFalse( $obj->isLinkDead( 'https://astraldynamics.com' ) );
+		$this->assertFalse( $obj->isLinkDead( 'http://napavalleyregister.com/news/napa-pipe-plant-loads-its-final-rail-car/article_695e3e0a-8d33-5e3b-917c-07a7545b3594.html' ) );
+		$this->assertFalse( $obj->isLinkDead( 'http://content.onlinejacc.org/cgi/content/full/41/9/1633' ) );
+		$this->assertFalse( $obj->isLinkDead( 'http://flysunairexpress.com/#about' ) );
+		$this->assertFalse( $obj->isLinkDead( 'http://кц.рф/ru/' ) );
+		// @codingStandardsIgnoreEnd
 	}
 
 	/**
-	 * Test a single dead link via HTTPS
+	 * Test dead links
 	 */
-	public function testDeadlinkTrueHTTPS() {
+	public function testIsLinkDeadTrue() {
 		$obj = new CheckIfDead();
-		$url = 'https://en.wikipedia.org/nothing';
-		$result = $obj->isLinkDead( $url );
-		$this->assertTrue( $result );
-	}
-
-	/**
-	 * Test a single dead link with a protocal relative URL
-	 */
-	public function testDeadlinkTrueNoProtocol() {
-		$obj = new CheckIfDead();
-		$url = '//en.wikipedia.org/nothing';
-		$result = $obj->isLinkDead( $url );
-		$this->assertTrue( $result );
-	}
-
-	/**
-	 * Test a single live link via HTTPS
-	 */
-	public function testDeadlinkFalseHTTPS() {
-		$obj = new CheckIfDead();
-		$url = 'https://en.wikipedia.org';
-		$result = $obj->isLinkDead( $url );
-		$this->assertFalse( $result );
-	}
-
-	/**
-	 * Test a single live link with a protocal relative URL
-	 */
-	public function testDeadlinkFalseNoProtocol() {
-		$obj = new CheckIfDead();
-		$url = '//en.wikipedia.org/wiki/Main_Page';
-		$result = $obj->isLinkDead( $url );
-		$this->assertFalse( $result );
-	}
-
-	/**
-	 * Test a single live link via FTP
-	 */
-	public function testDeadlinkFalseFTP() {
-		$obj = new CheckIfDead();
-		$url = 'ftp://ftp.rsa.com/pub/pkcs/ascii/layman.asc';
-		$result = $obj->isLinkDead( $url );
-		$this->assertFalse( $result );
+		// @codingStandardsIgnoreStart Line exceeds 100 characters
+		$this->assertTrue( $obj->isLinkDead( 'https://en.wikipedia.org/nothing' ) );
+		$this->assertTrue( $obj->isLinkDead( '//en.wikipedia.org/nothing' ) );
+		$this->assertTrue( $obj->isLinkDead( 'http://worldchiropracticalliance.org/resources/greens/green4.htm' ) );
+		$this->assertTrue( $obj->isLinkDead( 'http://www.copart.co.uk/c2/specialSearch.html?_eventId=getLot&execution=e1s2&lotId=10543580' ) );
+		$this->assertTrue( $obj->isLinkDead( 'http://forums.lavag.org/Industrial-EtherNet-EtherNet-IP-t9041.html' ) );
+		$this->assertTrue( $obj->isLinkDead( 'http://203.221.255.21/opacs/TitleDetails?displayid=137394&collection=all&displayid=0&fieldcode=2&from=BasicSearch&genreid=0&ITEMID=$VARS.getItemId()&original=$VARS.getOriginal()&pageno=1&phrasecode=1&searchwords=Lara%20Saint%20Paul%20&status=2&subjectid=0&index=' ) );
+		// @codingStandardsIgnoreEnd
 	}
 
 	/**
 	 * Test an array of dead links
 	 */
-	public function testDeadlinksTrue() {
-		$obj = new CheckIfDead();
-		// @codingStandardsIgnoreStart Line exceeds 100 characters
-		$urls = [
-			'https://en.wikipedia.org/nothing',
-			'http://www.copart.co.uk/c2/specialSearch.html?_eventId=getLot&execution=e1s2&lotId=10543580',
-			'http://forums.lavag.org/Industrial-EtherNet-EtherNet-IP-t9041.html',
-			'http://203.221.255.21/opacs/TitleDetails?displayid=137394&collection=all&displayid=0&fieldcode=2&from=BasicSearch&genreid=0&ITEMID=$VARS.getItemId()&original=$VARS.getOriginal()&pageno=1&phrasecode=1&searchwords=Lara%20Saint%20Paul%20&status=2&subjectid=0&index='
-		];
-		// @codingStandardsIgnoreEnd
-		$result = $obj->areLinksDead( $urls );
-		$expected = [ true, true, true, true ];
-		$this->assertEquals( $expected, array_values( $result ) );
-	}
-
-	/**
-	 * Test an array of live links
-	 */
-	public function testDeadlinksFalse() {
+	public function testAreLinksDead() {
 		$obj = new CheckIfDead();
 		// @codingStandardsIgnoreStart Line exceeds 100 characters
 		$urls = [
 			'https://en.wikipedia.org/wiki/Main_Page',
-			'https://en.wikipedia.org/w/index.php?title=Republic_of_India',
-			'https://astraldynamics.com',
-			'http://news.bbc.co.uk/2/hi/uk_news/england/coventry_warwickshire/6236900.stm',
-			'http://napavalleyregister.com/news/napa-pipe-plant-loads-its-final-rail-car/article_695e3e0a-8d33-5e3b-917c-07a7545b3594.html',
-			'http://content.onlinejacc.org/cgi/content/full/41/9/1633',
-			'http://flysunairexpress.com/#about',
-			'ftp://ftp.rsa.com/pub/pkcs/ascii/layman.asc'
+			'https://en.wikipedia.org/nothing',
 		];
 		// @codingStandardsIgnoreEnd
 		$result = $obj->areLinksDead( $urls );
-		$expected = [ false, false, false, false, false, false, false, false ];
+		$expected = [ false, true ];
 		$this->assertEquals( $expected, array_values( $result ) );
 	}
 
 	/**
 	 * Test the URL cleaning function
 	 */
-	public function testCleanUrl() {
+	public function testCleanURL() {
 		$obj = new CheckIfDead();
-		$this->assertEquals( $obj->cleanUrl( 'http://google.com?q=blah' ), 'google.com?q=blah' );
-		$this->assertEquals( $obj->cleanUrl( 'https://www.google.com/' ), 'google.com' );
-		$this->assertEquals( $obj->cleanUrl( 'ftp://google.com/#param=1' ), 'google.com' );
-		$this->assertEquals( $obj->cleanUrl( '//google.com' ), 'google.com' );
-		$this->assertEquals( $obj->cleanUrl( 'www.google.www.com' ), 'google.www.com' );
+		// @codingStandardsIgnoreStart Line exceeds 100 characters
+		$this->assertEquals( $obj->cleanURL( 'http://google.com?q=blah' ), 'google.com?q=blah' );
+		$this->assertEquals( $obj->cleanURL( 'https://www.google.com/' ), 'google.com' );
+		$this->assertEquals( $obj->cleanURL( 'ftp://google.com/#param=1' ), 'google.com' );
+		$this->assertEquals( $obj->cleanURL( '//google.com' ), 'google.com' );
+		$this->assertEquals( $obj->cleanURL( 'www.google.www.com' ), 'google.www.com' );
+		// @codingStandardsIgnoreEnd
 	}
 
+	/**
+	 * Test the URL sanitizing function
+	 */
+	public function testSanitizeURL() {
+		$obj = new CheckIfDead();
+		// @codingStandardsIgnoreStart Line exceeds 100 characters
+		$this->assertEquals( $obj->sanitizeURL( 'http://google.com?q=blah' ), 'http://google.com/?q=blah' );
+		$this->assertEquals( $obj->sanitizeURL( '//google.com?q=blah' ), 'https://google.com/?q=blah' );
+		$this->assertEquals( $obj->sanitizeURL( 'ftp://google.com/#param=1' ), 'ftp://google.com/#param=1' );
+		$this->assertEquals( $obj->sanitizeURL( 'https://zh.wikipedia.org/wiki/猫' ), 'https://zh.wikipedia.org/wiki/%E7%8C%AB' );
+		$this->assertEquals( $obj->sanitizeURL( 'http://www.discogs.com/Various-Kad-Jeknu-Dragačevske-Trube-2' ), 'http://www.discogs.com/Various-Kad-Jeknu-Draga%C4%8Devske-Trube-2' );
+		$this->assertEquals( $obj->sanitizeURL( 'http://кц.рф/ru/' ), 'http://xn--j1ay.xn--p1ai/ru/' );
+		// @codingStandardsIgnoreEnd
+	}
+
+	/**
+	 * Test the URL parsing function
+	 */
+	public function testParseURL() {
+		$obj = new CheckIfDead();
+		// @codingStandardsIgnoreStart Line exceeds 100 characters
+		$this->assertEquals( $obj->parseURL( 'http://кц.рф/ru/', PHP_URL_SCHEME ), 'http' );
+		$this->assertEquals( $obj->parseURL( 'http://кц.рф/ru/', PHP_URL_HOST ), 'кц.рф' );
+		$this->assertEquals( $obj->parseURL( 'http://кц.рф/ru/', PHP_URL_PATH ), '/ru/' );
+		$this->assertEmpty( $obj->parseURL( 'http://кц.рф/ru/', PHP_URL_USER ) );
+		$this->assertEmpty( $obj->parseURL( 'http://кц.рф/ru/', PHP_URL_PASS ) );
+		$this->assertEmpty( $obj->parseURL( 'http://кц.рф/ru/', PHP_URL_PORT ) );
+		$this->assertEmpty( $obj->parseURL( 'http://кц.рф/ru/', PHP_URL_QUERY ) );
+		$this->assertEmpty( $obj->parseURL( 'http://кц.рф/ru/', PHP_URL_FRAGMENT ) );
+		$this->assertEquals( $obj->parseURL( 'http://кц.рф/ru/' ), array( 'scheme' => 'http', 'host' => 'кц.рф', 'path' => '/ru/' ) );
+		$this->assertEquals( $obj->parseURL( 'http://www.discogs.com/Various-Kad-Jeknu-Dragačevske-Trube-2' ), array( 'scheme' => 'http', 'host' => 'www.discogs.com', 'path' => '/Various-Kad-Jeknu-Dragačevske-Trube-2' ) );
+		// @codingStandardsIgnoreEnd
+	}
 }
