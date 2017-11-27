d89a6d017d24d46eb981da3dc0bbcea39b2de607
kaldari <kaldari@gmail.com>
2017-11-27 13:44:48 -0800

    update userAgent and fix codingStandardsIgnore comments

--- src/CheckIfDead.php
+++ src/CheckIfDead.php
@@ -15,15 +15,13 @@ class CheckIfDead {
 	 * UserAgent for the device/browser we are pretending to be
 	 */
 	// @codingStandardsIgnoreStart Line exceeds 100 characters
-	protected $userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
+	protected $userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36";
+	// @codingStandardsIgnoreEnd
 
 	/**
 	 * UserAgent for the media player we are pretending to be
 	 */
-	// @codingStandardsIgnoreStart Line exceeds 100 characters
 	protected $mediaAgent = "Windows-Media-Player/12.0.15063.608";
-
-	// @codingStandardsIgnoreEnd
 
 	/**
 	 *  HTTP/RTSP/MMS codes that do not indicate a dead link
